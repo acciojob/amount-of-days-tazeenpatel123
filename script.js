0484a7e6347ebaa2674f56df8329dc660c657dba
tazeenpatel123 <150174275+tazeenpatel123@users.noreply.github.com>
2023-12-10 20:19:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+	if (year % 4===0 && year%100 !==0 || year % 400===0) {
+		return 366;//leap year
+	}else{
+		return 365// non-leap year
+	}
+}
